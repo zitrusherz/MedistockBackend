10d4d31ee63fed4961cf1f4f5486f9460bb6323a
Christopher Andrés Euribe Euribe <ch.euribe@duocuc.cl>
2026-05-25 21:51:06 -0400

    Eliminar comentario innecesario en admin.py

--- apps/inventory/admin.py
+++ apps/inventory/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.utils.html import format_html, mark_safe  # ← añadir mark_safe
+from django.utils.html import format_html, mark_safe
 from .models import (
     Categoria, Marca, Producto, CategoriaProducto,
     Lote, Inventario, MovimientoInventario,
