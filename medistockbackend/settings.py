3433e1f51b33af53d081e1776bc6341c517adf8e
Christopher Andrés Euribe Euribe <zitrusherz@gmail.com>
2026-07-03 10:38:56 -0400

    update

--- medistockbackend/settings.py
+++ medistockbackend/settings.py
@@ -182,7 +182,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 MEDIA_URL  = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
-
+STATIC_ROOT = BASE_DIR / ('static'
+                          '')
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
